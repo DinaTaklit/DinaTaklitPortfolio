2b655011522e970adfde9637824f7d317e0deec6
DinaTaklit <ez_taklit@esi.dz>
2020-10-08 04:54:08 +0100

    portfolio: compilation

---
+++ src/public/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    "use strict";
+    $(".pCard_add").click(function () {
+      $(".pCard_card").toggleClass("pCard_on");
+      $(".pCard_add i").toggleClass("fa-minus");
+    });
+  });
+
+
+
+
+
