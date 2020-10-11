31321b7992a8c6a0eca7a46c21a00cf4c49f34f3
DinaTaklit <ez_taklit@esi.dz>
2020-10-11 07:07:33 +0100

    social activites: carousel add js content

--- src/public/js/social-activities.js
+++ src/public/js/social-activities.js
@@ -1,3 +1,4 @@
+/** Social activities organizations**/
 const items = Array.from(document.getElementsByClassName('activity-dynamic-item'));
 console.log("items here");
 console.log(items);
@@ -12,3 +13,65 @@ items.forEach((item,i) => {
 })
 
 items[0].classList.add('animate');
+
+/*** Social activities carousel***/
+$('#activities-carousel').carousel({
+    interval: false
+  });
+  $('#carousel-thumbs').carousel({
+    interval: false
+  });
+
+  // handles the carousel thumbnails
+  // https://stackoverflow.com/questions/25752187/bootstrap-carousel-with-thumbnails-multiple-carousel
+  $('[id^=carousel-selector-]').click(function() {
+    var id_selector = $(this).attr('id');
+    var id = parseInt( id_selector.substr(id_selector.lastIndexOf('-') + 1) );
+    $('#activities-carousel').carousel(id);
+  });
+  // Only display 3 items in nav on mobile.
+  if ($(window).width() < 575) {
+    $('#carousel-thumbs .row div:nth-child(4)').each(function() {
+      var rowBoundary = $(this);
+      $('<div class="row mx-0">').insertAfter(rowBoundary.parent()).append(rowBoundary.nextAll().addBack());
+    });
+    $('#carousel-thumbs .carousel-item .row:nth-child(even)').each(function() {
+      var boundary = $(this);
+      $('<div class="carousel-item">').insertAfter(boundary.parent()).append(boundary.nextAll().addBack());
+    });
+  }
+  // Hide slide arrows if too few items.
+  if ($('#carousel-thumbs .carousel-item').length < 2) {
+    $('#carousel-thumbs [class^=carousel-control-]').remove();
+    $('.machine-carousel-container #carousel-thumbs').css('padding','0 5px');
+  }
+  // when the carousel slides, auto update
+  $('#activities-carousel').on('slide.bs.carousel', function(e) {
+    var id = parseInt( $(e.relatedTarget).attr('data-slide-number') );
+    $('[id^=carousel-selector-]').removeClass('selected');
+    $('[id=carousel-selector-'+id+']').addClass('selected');
+  });
+  // when user swipes, go next or previous
+  $('#activities-carousel').swipe({
+    fallbackToMouseEvents: true,
+    swipeLeft: function(e) {
+      $('#activities-carousel').carousel('next');
+    },
+    swipeRight: function(e) {
+      $('#activities-carousel').carousel('prev');
+    },
+    allowPageScroll: 'vertical',
+    preventDefaultEvents: false,
+    threshold: 75
+  });
+  /*
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+  */
+
+  $('#activities-carousel .carousel-item img').on('click', function(e) {
+    var src = $(e.target).attr('data-remote');
+    if (src) $(this).ekkoLightbox();
+  });
