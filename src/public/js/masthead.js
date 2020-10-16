196ba3f4e96caae81a5df19febde763a3d90bdad
DinaTaklit <ez_taklit@esi.dz>
2020-10-16 05:42:17 +0100

    update the first version of code

--- src/public/js/masthead.js
+++ src/public/js/masthead.js
@@ -1,40 +1,40 @@
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.sub-heading-role .line',
-//     opacity: [0.5,1],
-//     scaleX: [0, 1],
-//     easing: "easeInOutExpo",
-//     duration: 700
-//   }).add({
-//     targets: '.sub-heading-role .line',
-//     duration: 600,
-//     easing: "easeOutExpo",
-//     translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
-//   }).add({
-//     targets: '.sub-heading-role .ampersand',
-//     opacity: [0,1],
-//     scaleY: [0.5, 1],
-//     easing: "easeOutExpo",
-//     duration: 600,
-//     offset: '-=600'
-//   }).add({
-//     targets: '.sub-heading-role .letters-left',
-//     opacity: [0,1],
-//     translateX: ["0.5em", 0],
-//     easing: "easeOutExpo",
-//     duration: 600,
-//     offset: '-=300'
-//   }).add({
-//     targets: '.sub-heading-role .letters-right',
-//     opacity: [0,1],
-//     translateX: ["-0.5em", 0],
-//     easing: "easeOutExpo",
-//     duration: 600,
-//     offset: '-=600'
-//   }).add({
-//     targets: '.sub-heading-role',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
+anime.timeline({loop: true})
+  .add({
+    targets: '.sub-heading-role .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.sub-heading-role .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.sub-heading-role .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.sub-heading-role .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.sub-heading-role .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.sub-heading-role',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
