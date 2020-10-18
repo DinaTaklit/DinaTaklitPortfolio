f5bae2b611a0a3af1279f11e0a3bb8ca87c2dbe0
DinaTaklit <ez_taklit@esi.dz>
2020-10-18 17:03:18 +0100

    Main.js: Add back to top code function

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -17,6 +17,23 @@
             }
         }
     });
+    //=============================================================
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+        } else {
+        $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        return false;
+    });
+    //=============================================================
 
 })(jQuery); // End of use strict
 
